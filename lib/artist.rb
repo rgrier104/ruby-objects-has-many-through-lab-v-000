c86d8ee3dae77c9ba02a45c4bb8473c2be2171af
Rachel Grier <rgrier104@gmail.com>
2018-09-28 13:06:18 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,21 @@
+class Artist
+
+  attr_accessor :name, :songs
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_song(name, genre)
+    song = Song.new(name,self,genre)
+    self.songs << song
+  end
+
+end
---
+++ lib/genre.rb
@@ -0,0 +1,16 @@
+class Genre
+
+  attr_accessor :name, :songs
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+end
