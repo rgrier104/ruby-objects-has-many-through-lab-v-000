82508d3c93a8b1a186be4b6c3a6d945a489b16ff
Rachel Grier <rgrier104@gmail.com>
2018-09-28 13:42:52 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -17,7 +17,7 @@ class Doctor
   end
 
   def appointments
-    Appointments.all.select do |appointment|
+    Appointment.all.select do |appointment|
       appointment.doctor == self
     end
   end
